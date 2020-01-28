c464ff4070c55ae9868833de7a0364c91d7b54b8
Fontana Canella <erni-services.bruno.fontana.canella@hp.com>
2020-01-28 09:17:51 +0100

    Initiation repo

---
+++ target/rls/debug/deps/sorts-041e62a8c388e2d5.d
@@ -0,0 +1,5 @@
+c:\Users\fontanac\Documents\git\rust\target\rls\debug\deps\sorts-041e62a8c388e2d5.rmeta: sorts\src\main.rs
+
+c:\Users\fontanac\Documents\git\rust\target\rls\debug\deps\sorts-041e62a8c388e2d5.d: sorts\src\main.rs
+
+sorts\src\main.rs:
---
+++ target/rls/debug/deps/sorts-470084e94d9f8e11.d
@@ -0,0 +1,5 @@
+c:\Users\fontanac\Documents\git\rust\target\rls\debug\deps\sorts-470084e94d9f8e11.rmeta: sorts\src\main.rs
+
+c:\Users\fontanac\Documents\git\rust\target\rls\debug\deps\sorts-470084e94d9f8e11.d: sorts\src\main.rs
+
+sorts\src\main.rs:
